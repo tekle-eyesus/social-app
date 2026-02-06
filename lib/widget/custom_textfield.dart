590d9a0c9f6150d253589c4c1cf039e6005ec7a5
tekle-eyesus <tekleeyesus21@gmail.com>
2026-02-06 00:36:48 -0700

    refactor CustomTextfield to manage obscureText state and improve functionality

--- lib/widget/custom_textfield.dart
+++ lib/widget/custom_textfield.dart
@@ -1,22 +1,36 @@
 import 'package:flutter/material.dart';
 
-class CustomTextfield extends StatelessWidget {
+class CustomTextfield extends StatefulWidget {
   final String hintText;
-  final bool obscureText;
+  bool obscureText;
   final TextEditingController controller;
-  const CustomTextfield(
+  CustomTextfield(
       {super.key,
       required this.hintText,
       required this.obscureText,
       required this.controller});
 
   @override
+  State<CustomTextfield> createState() => _CustomTextfieldState();
+}
+
+class _CustomTextfieldState extends State<CustomTextfield> {
+  _toggleObscureText() {
+    if (widget.obscureText) {
+      widget.obscureText = false;
+    } else {
+      widget.obscureText = true;
+    }
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
     return TextField(
-      controller: controller,
-      obscureText: obscureText,
+      controller: widget.controller,
+      obscureText: widget.obscureText,
       decoration: InputDecoration(
-        hintText: hintText,
+        hintText: widget.hintText,
         hintStyle: const TextStyle(
           color: Colors.grey,
         ),
@@ -26,6 +40,15 @@ class CustomTextfield extends StatelessWidget {
           ),
           borderRadius: BorderRadius.circular(12),
         ),
+        suffixIcon: widget.hintText.contains("Password")
+            ? IconButton(
+                onPressed: _toggleObscureText,
+                icon: const Icon(
+                  Icons.visibility_off,
+                  color: Colors.grey,
+                ),
+              )
+            : null,
         enabledBorder: OutlineInputBorder(
           borderSide: BorderSide(
             color: Colors.black.withOpacity(0.2),
