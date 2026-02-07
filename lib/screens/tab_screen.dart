f83710b157c34ca24016e70dec205b9f6bf6d7d7
tekle-eyesus <tekleeyesus21@gmail.com>
2026-02-07 02:29:03 -0700

    style: refactor TabScreen to use BottomNavigationBar with improved dark mode support and updated item styling

--- lib/screens/tab_screen.dart
+++ lib/screens/tab_screen.dart
@@ -1,5 +1,3 @@
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:socialapp/screens/HomeScreen.dart';
@@ -16,61 +14,106 @@ class TabScreen extends StatefulWidget {
 }
 
 class _TabScreenState extends State<TabScreen> {
-  var index = 0;
+  int _currentIndex = 0;
 
-  final screens = [
-    HomeScreen(),
-    UsersScreen(),
+  final List<Widget> _screens = [
+    const HomeScreen(),
+    const UsersScreen(),
     PostScreen(),
-    NotifyScreen(),
-    ProfileScreen(),
+    const NotifyScreen(),
+    const ProfileScreen(),
   ];
+
   @override
   Widget build(BuildContext context) {
+    final isDarkMode = Theme.of(context).brightness == Brightness.dark;
+    const unselectedItemColor = Colors.grey;
+    final backgroundColor = isDarkMode
+        ? const Color(0xFF000000)
+        : const Color(
+            0xFFFFFFFF,
+          );
+    final selectedItemColor = isDarkMode
+        ? const Color(0xFFFFFFFF)
+        : const Color(
+            0xFF0F1419,
+          );
+
     return Scaffold(
-      extendBody: true,
-      bottomNavigationBar: CurvedNavigationBar(
-        index: index,
-        height: 55,
-        color: Colors.blue.shade400,
-        backgroundColor: Colors.transparent,
-        buttonBackgroundColor: const Color.fromARGB(255, 6, 16, 88),
-        animationDuration: const Duration(milliseconds: 300),
-        items: const [
-          FaIcon(
-            FontAwesomeIcons.home,
-            size: 22,
-            color: Colors.white,
-          ),
-          FaIcon(
-            FontAwesomeIcons.users,
-            size: 22,
-            color: Colors.white,
-          ),
-          FaIcon(
-            FontAwesomeIcons.add,
-            size: 26,
-            color: Color.fromARGB(255, 95, 3, 165),
-          ),
-          FaIcon(
-            FontAwesomeIcons.solidBell,
-            size: 22,
-            color: Colors.white,
-          ),
-          FaIcon(
-            FontAwesomeIcons.userAstronaut,
-            size: 22,
-            color: Colors.white,
-          ),
-        ],
-        onTap: (index) {
-          //Handle button tap
-          setState(() {
-            this.index = index;
-          });
-        },
+      body: IndexedStack(
+        index: _currentIndex,
+        children: _screens,
       ),
-      body: screens[index],
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          border: Border(
+            top: BorderSide(
+              color: isDarkMode ? Colors.grey.shade900 : Colors.grey.shade300,
+              width: 0.5,
+            ),
+          ),
+        ),
+        child: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: backgroundColor,
+          selectedItemColor: selectedItemColor,
+          unselectedItemColor: unselectedItemColor,
+          showSelectedLabels: false, // X style: hide labels
+          showUnselectedLabels: false,
+          elevation: 0,
+          items: [
+            const BottomNavigationBarItem(
+              icon: FaIcon(FontAwesomeIcons.house, size: 24),
+              activeIcon: FaIcon(FontAwesomeIcons.house, size: 24),
+              label: 'Home',
+            ),
+            const BottomNavigationBarItem(
+              icon: FaIcon(FontAwesomeIcons.users, size: 24),
+              activeIcon: FaIcon(FontAwesomeIcons.users, size: 24),
+              label: 'Users',
+            ),
+            BottomNavigationBarItem(
+              icon: Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                decoration: BoxDecoration(
+                    color: _currentIndex == 2
+                        ? (isDarkMode ? Colors.white : Colors.black)
+                        : Colors.transparent,
+                    borderRadius: BorderRadius.circular(12),
+                    border: Border.all(
+                        color: isDarkMode ? Colors.white : Colors.black,
+                        width: 1.5)),
+                child: FaIcon(
+                  FontAwesomeIcons.plus,
+                  size: 18,
+                  // Invert color if selected
+                  color: _currentIndex == 2
+                      ? (isDarkMode ? Colors.black : Colors.white)
+                      : (isDarkMode ? Colors.white : Colors.black),
+                ),
+              ),
+              label: 'Post',
+            ),
+            const BottomNavigationBarItem(
+              icon: FaIcon(FontAwesomeIcons.bell, size: 24),
+              activeIcon: FaIcon(FontAwesomeIcons.solidBell, size: 24),
+              label: 'Notifications',
+            ),
+            const BottomNavigationBarItem(
+              icon: FaIcon(FontAwesomeIcons.user, size: 24),
+              activeIcon: FaIcon(FontAwesomeIcons.solidUser, size: 24),
+              label: 'Profile',
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
