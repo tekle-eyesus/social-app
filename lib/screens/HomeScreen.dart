3e03a17a77b6a39392606eeb1d307ba93dd991bf
tekle-eyesus <tekleeyesus21@gmail.com>
2026-02-19 08:50:49 -0700

    refactor

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -5,6 +5,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:socialapp/helpers/helper_functions.dart';
 import 'package:socialapp/helpers/snackbar_helper.dart';
 import 'package:socialapp/theme/app_colors.dart';
+import 'package:socialapp/widget/offline_page.dart';
 import 'package:socialapp/widget/post_card.dart';
 import 'package:socialapp/widget/post_shimmer.dart';
 
@@ -144,47 +145,7 @@ class _HomeScreenState extends State<HomeScreen> {
               : CustomSnackBar.showError(context, "Lost Connection!");
         },
         offlineBuilder: (context) {
-          return Scaffold(
-            appBar: AppBar(
-              backgroundColor: AppColors.surface,
-              foregroundColor: AppColors.primaryText,
-              leading: const Row(
-                children: [
-                  SizedBox(
-                    width: 6,
-                  ),
-                  CircleAvatar(
-                    radius: 16,
-                    backgroundImage: AssetImage(
-                      'assets/icon/icon.png',
-                    ),
-                  ),
-                ],
-              ),
-              title: const Text(
-                "WeTok",
-                style: TextStyle(
-                    fontSize: 22,
-                    fontFamily: 'Poppins',
-                    fontWeight: FontWeight.bold,
-                    color: Color.fromARGB(255, 21, 45, 81)),
-              ),
-            ),
-            body: Container(
-              color: Colors.red.shade50,
-              width: double.infinity,
-              child: const Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Icon(Icons.wifi_off, size: 100, color: Colors.red),
-                  SizedBox(height: 20),
-                  Text("No Internet Connection",
-                      style: TextStyle(fontSize: 24)),
-                  Text("Please check your settings."),
-                ],
-              ),
-            ),
-          );
+          return const OfflinePage();
         },
       ),
     );
