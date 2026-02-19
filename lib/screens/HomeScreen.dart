a1a9ed0906a893475e872b7496ede13333dad97c
tekle-eyesus <tekleeyesus21@gmail.com>
2026-02-19 08:47:05 -0700

    style

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -144,17 +144,45 @@ class _HomeScreenState extends State<HomeScreen> {
               : CustomSnackBar.showError(context, "Lost Connection!");
         },
         offlineBuilder: (context) {
-          return Container(
-            color: Colors.red.shade50,
-            width: double.infinity,
-            child: const Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                Icon(Icons.wifi_off, size: 100, color: Colors.red),
-                SizedBox(height: 20),
-                Text("No Internet Connection", style: TextStyle(fontSize: 24)),
-                Text("Please check your settings."),
-              ],
+          return Scaffold(
+            appBar: AppBar(
+              backgroundColor: AppColors.surface,
+              foregroundColor: AppColors.primaryText,
+              leading: const Row(
+                children: [
+                  SizedBox(
+                    width: 6,
+                  ),
+                  CircleAvatar(
+                    radius: 16,
+                    backgroundImage: AssetImage(
+                      'assets/icon/icon.png',
+                    ),
+                  ),
+                ],
+              ),
+              title: const Text(
+                "WeTok",
+                style: TextStyle(
+                    fontSize: 22,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(255, 21, 45, 81)),
+              ),
+            ),
+            body: Container(
+              color: Colors.red.shade50,
+              width: double.infinity,
+              child: const Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Icon(Icons.wifi_off, size: 100, color: Colors.red),
+                  SizedBox(height: 20),
+                  Text("No Internet Connection",
+                      style: TextStyle(fontSize: 24)),
+                  Text("Please check your settings."),
+                ],
+              ),
             ),
           );
         },
