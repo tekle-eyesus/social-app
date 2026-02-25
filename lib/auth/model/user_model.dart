ecaecfdae7dc76b11a6702f6e5fc3196a8352a5b
tekle-eyesus <tekleeyesus21@gmail.com>
2026-02-25 06:49:22 -0700

    feat: add UserModel for user data management with Firestore integration

---
+++ lib/auth/model/user_model.dart
@@ -0,0 +1,49 @@
+class UserModel {
+  final String uid;
+  final String email;
+  final String username;
+  final String profession;
+  final String profilePic;
+  final String bio;
+  final int followersCount;
+  final int followingCount;
+
+  UserModel({
+    required this.uid,
+    required this.email,
+    required this.username,
+    required this.profession,
+    required this.profilePic,
+    this.bio = "",
+    this.followersCount = 0,
+    this.followingCount = 0,
+  });
+
+  // Factory: Create a UserModel from a Firestore Map
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(
+      uid: map['uid'] ?? '',
+      email: map['email'] ?? '',
+      username: map['username'] ?? '',
+      profession: map['profession'] ?? '',
+      profilePic: map['profilePic'] ?? '',
+      bio: map['bio'] ?? '',
+      followersCount: map['followersCount'] ?? 0,
+      followingCount: map['followingCount'] ?? 0,
+    );
+  }
+
+  // ToMap: Convert UserModel to Map for saving to Firestore
+  Map<String, dynamic> toMap() {
+    return {
+      'uid': uid,
+      'email': email,
+      'username': username,
+      'profession': profession,
+      'profilePic': profilePic,
+      'bio': bio,
+      'followersCount': followersCount,
+      'followingCount': followingCount,
+    };
+  }
+}
